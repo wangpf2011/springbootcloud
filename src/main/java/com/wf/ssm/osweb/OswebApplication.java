bbe895bedd25099f058a8d0779c4078d3f63c383
wangpf <wangpf2011@163.com>
2019-04-01 19:31:30 +0800

    init commit

---
+++ src/main/java/com/wf/ssm/osweb/OswebApplication.java
@@ -0,0 +1,42 @@
+package com.wf.ssm.osweb;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class OswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OswebApplication.class, args);
+	}
+
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/rest")
+    @ResponseBody
+    public Map<String, Object> rest() {
+
+        Map<String, Object> data = new HashMap<String, Object>();
+
+        data.put("1", "A");
+        data.put("2", 2);
+
+        return data;
+    }
+}
